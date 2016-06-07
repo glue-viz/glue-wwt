f028f69da4c2fc81428446f898e453a42e8cc0ce
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-06-07 12:14:01 +0100

    Fix plugin setup

--- glue_wwt/__init__.py
+++ glue_wwt/__init__.py
@@ -3,4 +3,5 @@ from __future__ import absolute_import, division, print_function
 
 def setup():
     from .viewer.data_viewer import WWTDataViewer
+    from glue.config import qt_client
     qt_client.add(WWTDataViewer)

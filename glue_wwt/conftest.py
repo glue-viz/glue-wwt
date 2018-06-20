b1874cdf2c598f1116f4daeefa1c4286fe26b831
Thomas Robitaille <thomas.robitaille@gmail.com>
2018-06-20 10:44:48 +0100

    Small improvements to testing

--- glue_wwt/conftest.py
+++ glue_wwt/conftest.py
@@ -1,12 +1,17 @@
 from __future__ import absolute_import, division, print_function
 
-from glue.utils.qt import get_qapp
-
 # The application has to always be referenced to avoid being shut down, so we
 # keep a reference to it here
 app = None
 
 
 def pytest_configure(config):
+
     global app
+
+    # We need to import PyWWT before setting up a QApplication since the
+    # WebEngine widgets require this.
+    from pywwt.qt import WWTQtClient  # noqa
+
+    from glue.utils.qt import get_qapp
     app = get_qapp()

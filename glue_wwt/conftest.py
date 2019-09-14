9c26fe177b8bee4ec9201c814fcb7fa8ee9e484a
Thomas Robitaille <thomas.robitaille@gmail.com>
2019-09-14 14:50:44 +0100

    Try and run Chromium in single process mode

--- glue_wwt/conftest.py
+++ glue_wwt/conftest.py
@@ -1,11 +1,15 @@
 from __future__ import absolute_import, division, print_function
 
+import os
+
 # The application has to always be referenced to avoid being shut down, so we
 # keep a reference to it here
 app = None
 
 
 def pytest_configure(config):
+
+    os.environ['QTWEBENGINE_CHROMIUM_FLAGS'] = '--single-process'
 
     global app
 

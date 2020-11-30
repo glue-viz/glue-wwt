1fbc96d0534dced13d077edd047e6f04a45e4395
Thomas Robitaille <thomas.robitaille@gmail.com>
2020-11-30 20:21:32 +0000

    Use standalone echo

--- glue_wwt/viewer/viewer_state.py
+++ glue_wwt/viewer/viewer_state.py
@@ -2,8 +2,8 @@ from __future__ import absolute_import, division, print_function
 
 from astropy import units as u
 
-from glue.external.echo import (CallbackProperty, ListCallbackProperty,
-                                SelectionCallbackProperty)
+from echo import (CallbackProperty, ListCallbackProperty,
+                  SelectionCallbackProperty)
 
 from glue.core.data_combo_helper import ComponentIDComboHelper
 from glue.viewers.common.state import ViewerState

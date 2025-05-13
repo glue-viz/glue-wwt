a3dbe6d4f1fd43085f7c496fb3dce40157a399f3
Thomas Robitaille <thomas.robitaille@gmail.com>
2025-05-13 23:25:39 +0100

    Added Jupyter entry point

--- glue_wwt/__init__.py
+++ glue_wwt/__init__.py
@@ -11,7 +11,11 @@ import importlib.metadata
 __version__ = importlib.metadata.version('glue-core')
 
 
-def setup():
+def setup_qt():
     from .viewer.qt.qt_data_viewer import WWTQtViewer
     from glue.config import qt_client
     qt_client.add(WWTQtViewer)
+
+
+def setup_jupyter():
+    from .viewer.jupyter.jupyter_viewer import WWTJupyterViewer  # noqa
